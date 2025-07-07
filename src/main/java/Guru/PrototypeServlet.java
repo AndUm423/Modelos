d4ade5b73aa6d71fe983e200659f4bad5d393e33
AndUm423 <edg-andres@hotmail.com>
2025-07-07 13:40:05 -0500

    Protot

--- src/main/java/Guru/PrototypeServlet.java
+++ src/main/java/Guru/PrototypeServlet.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package Guru;
 
 import jakarta.servlet.RequestDispatcher;
@@ -12,33 +8,50 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author camiloapaezg
- */
-@WebServlet(name = "SingletonServlet", urlPatterns = {"/SingletonServlet"})
-public class SingletonServlet extends HttpServlet {
+@WebServlet(name = "PrototypeServlet", urlPatterns = {"/PrototypeServlet"})
+public class PrototypeServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
         RequestDispatcher dispatcher;
-        dispatcher = request.getRequestDispatcher("/Patrones/Singleton.jsp");
+        dispatcher = request.getRequestDispatcher("/Patrones/Prototype.jsp");
         dispatcher.forward(request, response);
     }
-   
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
     @Override
     public String getServletInfo() {
         return "Short description";
